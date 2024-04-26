3482115d9759c7c2da65755a80dfd2c7706523f7
!ᗰ A@m!R <153732158+imaamir0347@users.noreply.github.com>
2024-04-26 22:10:02 +0530

    matrix program

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+vector<int> shuffle(vector<int>& nums, int n)
+{
+    vector<int>arr;
+    int start=0;
+    int end=n;
+    for(int i=0; i<nums.size(); i++)
+    {
+        if(i/2==0)
+        {
+
+            arr[i]=nums[start++];
+        }
+        else
+        {
+            arr[i]=nums[end++];
+        }
+
+    }
+    return arr;
+}
+int main()
+{
+    int arr[]= {1,2,3,3,2,1};
+    int arr2[6];
+    arr2=shuffle(arr,3);
+    for(int i=0; i<6; i++)
+    {
+        cout<<arr2[i]<<" ";
+    }
+    return 0;
+}
