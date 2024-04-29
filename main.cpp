c8677f3deb74919a6a1e2c2ed5827119f33a3e74
imaamir0347 <aamirthegreat0437@gmail.com>
2024-04-29 22:58:37 +0530

    commiting

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num=120.121;
+
+
+    /*char name='A';
+    float num2=120.12;
+    double num3=121.125;
+
+    cout << num << endl;
+    cout << num2 << endl;
+    cout << num3 << endl;
+    cout<< "Character:"<<name<<endl;
+    cout << "Hello world!\n" ;
+    cout << "Hello world!" ;*/
+    //cout<<"Enter an integer,character,float and double:";
+    //cin>>num>>ch>>f>>d;
+    //cout<< "The number:" <<num<<"\nCharacter:"<<ch<<"\nFloat:"<<f<<"\nDouble:"<<d;
+    cout<<"number="<<num<<endl;
+    int f1=int(num);
+    cout<<"number="<<f1<<endl;
+    int f2=(int)num;
+    cout<<"number="<<f2<<endl;
+    return 0;
+}
